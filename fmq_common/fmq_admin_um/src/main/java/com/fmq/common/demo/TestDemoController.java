388cde04e2992eb06ec40cf05ddc006da095d6a6
Jerry1104 <liujianguo1104@163.com>
2017-11-30 08:43:32 +0800

    up yml

--- fmq_common/fmq_admin_um/src/main/java/com/fmq/common/demo/TestDemoController.java
+++ fmq_common/fmq_admin_um/src/main/java/com/fmq/common/demo/TestDemoController.java
@@ -1,13 +1,15 @@
 package com.fmq.common.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fmq.common.base.BaseController;
 import com.fmq.common.dto.TestDemo;
 
 @RestController
 @RequestMapping("/demo")
-public class TestDemoController {
+public class TestDemoController extends BaseController{
 
 	 /**
      * 返回demo数据:
@@ -15,10 +17,13 @@ public class TestDemoController {
      * @return
      */
     @RequestMapping("/getDemo")
+   // @ResponseBody
     public TestDemo getDemo(){
+    	logger.info("++++++++++++");
        TestDemo demo = new TestDemo();
        demo.setId(1);
        demo.setName("Angel");
+     	logger.info("++++++++++++"+demo.toString());
        return demo;
     }
 	
