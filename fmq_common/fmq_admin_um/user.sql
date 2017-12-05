fceb3d645a858b0902314cc7066f308006967494
Jerry1104 <liujianguo1104@163.com>
2017-12-05 20:29:13 +0800

    up

--- fmq_common/fmq_admin_um/user.sql
+++ fmq_common/fmq_admin_um/user.sql
@@ -1,10 +1,10 @@
 #用户表
-CREATE TABLE IF NOT EXISTS `UserInfo`(
+CREATE TABLE IF NOT EXISTS `fmq_user`(
        `id` INT UNSIGNED AUTO_INCREMENT,
-       `userName` VARCHAR(100) NOT NULL,
-       `passWord` VARCHAR(40) NOT NULL,
+       `user_Name` VARCHAR(100) NOT NULL,
+       `pass_Word` VARCHAR(40) NOT NULL,
        `create_date` DATE,
        PRIMARY KEY ( `id` )
      )ENGINE=InnoDB DEFAULT CHARSET=utf8;
      
-INSERT INTO UserInfo(userName,passWord) VALUES ('1','123');
+INSERT INTO fmq_user(user_Name,pass_Word) VALUES ('1','123');
