0dd67bdea2d64053aa210d4fe2bcfcf48d6defa7
Jerry1104 <liujianguo1104@163.com>
2017-12-12 20:10:09 +0800

    增加前后端请求

--- fmq_cms/src/components/Login.vue
+++ fmq_cms/src/components/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div class="login"> 
     <h1>{{ msg }}</h1>
     <ul>
        <div class="footer">
@@ -7,17 +7,17 @@
           <table align="center">
             <tr>
                <td class="fr"><label>用户名：</label></td>
-               <td><input type="text" placeholder="用户名" maxlength="40" class="loginName"/></td>
+               <td><input type="text" placeholder="用户名" maxlength="40" v-model="loginName" class="loginName"/></td>
             </tr>
             <tr>
               <td class="fr"><label>密码：</label></td>
-              <td><input type="password"  placeholder="密码" maxlength="40" class="loginPwd"/></td>
+              <td><input type="password"  placeholder="密码" maxlength="40"  v-model="loginPwd" class="loginPwd"/></td>
             </tr>
           </table>
         </div>
         <div class="error"></div>
         <div>
-          <a href="javascript:void(0);" class="login">登录</a>
+          <a href="javascript:void(0);" class="login" @click='login'>登录</a>
         </div>
       </div>
     </ul>
@@ -27,15 +27,43 @@
 
 
 <script>
+
+
+
 export default {
-  name: 'login',
+  name: 'Login',
   data () {
     return {
-      msg: '欢迎使用后台管理系统'
+      msg: '欢迎使用后台管理系统',
+      loginName:'',
+      loginPwd:''
+
     }
-  }
+
+
+  },
+    methods: {
+       login () {
+        var  self= this;
+         var pams={
+            account:self.loginName || 'abc',
+            password:self.loginPwd || 123
+        }
+         
+         this.$ajax({
+          method: 'get',
+          url: 'http://localhost:8081/loginPost',
+          data: JSON.stringify(pams)
+         }).then(function(e){
+            console.info(e);
+         })
+        
+
+      }
+}
 }
 </script>
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
--- fmq_cms/src/router/index.js
+++ fmq_cms/src/router/index.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
 
+
+
 Vue.use(Router)
+
 
 export default new Router({
   routes: [
